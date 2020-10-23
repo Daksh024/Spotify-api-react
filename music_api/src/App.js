4520e5f3d8de7637218775fe598b71074d3d8a25
Daksh024 <sounkk024@gmail.com>
2020-10-24 01:14:11 +0530

    search result is 15% done.

--- music_api/src/App.js
+++ music_api/src/App.js
@@ -1,19 +1,14 @@
 import React, {Component} from 'react';
 import Searchbar from './components/searchbar';
-import Searchresult from './components/searchresult';
-
-// let accessToken;
 
 class App extends Component{
 
   render(){
     return <div className="MainContent">
-      <div className="navbar navbar-expand-lg navbar-dark bg-secondary justify-content-between">
+      <div className="navbar navbar-expand-lg navbar-dark bg-secondary justify-content-center">
         <a className="navbar-brand" href=".">My Music</a>
-        <Searchbar/>
       </div>
-      <br/>
-      <Searchresult/>
+      <Searchbar/>
     </div>
   }
 
